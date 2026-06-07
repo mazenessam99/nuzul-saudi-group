ca29e0bf4a1e91f5662de41da99ae7651d2d9717
Mazen Essam <emazen628@gmail.com>
2026-06-07 13:10:51 +0300

    feat: enhance NotFound page with 404 message and animation

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 
-
+import { motion } from "framer-motion";
 export default function NotFound() {
     return (
-        <div className="">NotFound</div>
+        <main className="min-h-[70vh] flex items-center justify-center text-center px-4">
+            <motion.section initial={{opacity:0,y:80,scale: 0.175}} animate={{opacity:1,y:0,scale: 1}} transition={{duration:0.5}} className="text-center" >
+                <div className="text-8xl font-bold text-gold mb-4">404</div>
+                <h1 className="text-2xl font-bold mb-2 ">الصفحة غير موجودة</h1>
+                <p className="text-muted-foreground mb-12">عذراً، لم نتمكن من العثور على ما تبحث عنه.</p>
+                <Link to={'/'} className="px-6 py-3 rounded-full font-bold text-charcoal text-center gradient-gold transition-all hover:shadow-xl">العودة للرئيسية</Link>
+            </motion.section>
+        </main>
     )
 }
