8ea1a7170f8d79c1bfb80e34bad30249648789c5
Mazen Essam <emazen628@gmail.com>
2026-07-24 06:08:56 +0300

    fix: adjust minimum height and text size for better layout in InnerHero component

--- src/components/shared/InnerHero.tsx
+++ src/components/shared/InnerHero.tsx
@@ -9,7 +9,7 @@ interface HeroProps {
 
 export default function InnerHero({ image, eyebrow, title, description }: HeroProps) {
     return (
-        <section className="relative h-[70vh] min-h-115 flex items-center overflow-hidden">
+        <section className="relative h-[70vh] min-h-95 flex items-center overflow-hidden">
             <img src={image} className='absolute inset-0 w-full h-full  object-cover' alt='aboutHero' />
             <div className='absolute inset-0 bg-linear-to-t from-black/90 via-black/60 to-black/40' />
             <div className="container mx-auto px-4 lg:px-8 relative z-10 text-white">
@@ -20,7 +20,7 @@ export default function InnerHero({ image, eyebrow, title, description }: HeroPr
                 >
                     <span className="inline-block font-bold text-xs text-gold px-4 py-2 rounded-full tracking-tight border border-gold/40 mb-4">{eyebrow}</span>
                     <h1 className="text-4xl md:text-6xl max-w-3xl mb-4 font-bold leading-tight">{title}</h1>
-                    <p className="text-white/85 text-xl max-w-2xl leading-relaxed">{description}</p>
+                    <p className="text-white/85 text-xl sm:text-2xl max-w-2xl leading-relaxed">{description}</p>
 
                 </motion.div>
 
